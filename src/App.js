63c043348645a694557bd41cf44248d462dbbd68
Jessica Lalanne <jess@MBPJL.local>
2018-08-31 09:15:07 +0200

    Add VenueInfo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./resources/styles.css";
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured/Featured";
+import VenueInfo from "./components/venue_info/VenueInfo";
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height: "1500px", background: "cornflowerblue" }}>
         <Header />
         <Featured />
+        <VenueInfo />
       </div>
     );
   }
